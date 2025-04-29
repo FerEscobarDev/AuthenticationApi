347b6d1a306a76e312e8adc911f6b24775c826a5
Fernando Escobar <jeiesc04@outlook.com>
2025-04-28 21:44:37 -0500

    Fix: Add Entities User and RefreshToken

---
+++ AuthenticationApi.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationApi.Domain.Entities
+{
+    public sealed class RefreshToken
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string TokenHash { get; set; } = default!;
+        public DateTime ExpiresAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool Revoked { get; set; } = false;
+
+        // Relación
+        public Guid UserId { get; set; }
+        public User User { get; set; } = default!;
+    }
+}
---
+++ AuthenticationApi.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace AuthenticationApi.Domain.Entities
+{
+    public sealed class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string UserName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public bool EmailConfirmed { get; set; } = false;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Relaciones
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+
+    }
+}
